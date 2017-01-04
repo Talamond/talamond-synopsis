23a6f0fd4eb15d699c4293a107c79f088ada0bf8
Jonathan Sweetman <truesweetman@gmail.com>
2017-01-03 23:43:31 -0500

    resume sections

--- src/fragments/resume/resumeSection.js
+++ src/fragments/resume/resumeSection.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export const Section = ({id, title, expanded, content, onClick}) =>
   <div className="resume-section">
-    <div className="section-title" onClick={() => onClick(id)}>{title}</div>
+    <div className="title-area" onClick={() => onClick(id)}>
+      <div className="title">{title}</div>
+      <div className="chevron-container"><span className={`chevron ${expanded ? 'bottom' : ''}`}/></div>
+    </div>
     <div className="section-content">{expanded ? content : null}</div>
   </div>;
